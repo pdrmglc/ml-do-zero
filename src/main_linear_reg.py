3c37caddd1d439136b8148bc9240a43376232608
pdrmglc <pedro.jeronimo@fiocruz.br>
2024-12-21 20:13:26 -0300

    Remove __pycache__ directory from repository

--- src/main_linear_reg.py
+++ src/main_linear_reg.py
@@ -1,6 +1,6 @@
 # %% Importando bibliotecas
 
-from RegLinear import CustomLinearRegression
+from models.RegLinear import CustomLinearRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
